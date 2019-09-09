f51c889ec64d473e77285729bf99b85e90f36973
m09 <142691+m09@users.noreply.github.com>
2019-09-09 11:41:54 +0200

    Fix keyboard interruption handling bug
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- formatml/__main__.py
+++ formatml/__main__.py
@@ -4,14 +4,13 @@ from formatml.pipelines.pipeline import parse_args
 def main() -> None:
     """CLI entry point of the formatml."""
     handler, kw_args, graceful_keyboard_interruption = parse_args()
-    handler(**kw_args)
     if graceful_keyboard_interruption:
         try:
-            handler(**vars(kw_args))
+            handler(**kw_args)
         except KeyboardInterrupt:
             return
     else:
-        handler(**vars(kw_args))
+        handler(**kw_args)
 
 
 if __name__ == "__main__":
